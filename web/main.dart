e75dd8f2f5b17f0be24a3f7588709fd12439c756
nshahan <nshahan@google.com>
2016-10-05 10:55:58 -0700

    some cleanup

--- web/main.dart
+++ web/main.dart
@@ -34,17 +34,19 @@ main() {
   foo.onChange.listen(useTemplate);
 }
 
-// Updates the content of the editor using a template.
-void useTemplate(Event _) {
-  SelectElement templateSelectElement =
-      document.getElementById("templateSelect");
-  int selectedIndex = templateSelectElement.selectedIndex;
+// Add an entry to the log.
+void appendToLog(double stardate, HtmlElement logEntryElement) {
+  logEntries[stardate] = logEntryElement;
+  window.localStorage[stardate.toString()] = logEntryElement.innerHtml;
+  displayLogEntry(stardate, logEntryElement);
+}
 
-  if (selectedIndex == 0) return;
-
-  quillEditor.deleteText(0, quillEditor.getLength());
-  String templateText = templates[templateSelectElement.selectedIndex - 1];
-  quillEditor.insertText(0, templateText, 'api');
+// Calculate the current stardate: <Year>.<Percentage of year completion>
+double calcStardate() {
+  DateTime now = new DateTime.now();
+  DateTime beginningOfYear = new DateTime(now.year);
+  int secondsThisYear = now.difference(beginningOfYear).inSeconds;
+  return now.year + secondsThisYear / secondsInAYear;
 }
 
 // copy html elements from the editor view and return them inside a new
@@ -56,22 +58,6 @@ HtmlElement captureEditorView() {
   logEntryElement.innerHtml = contentElement.innerHtml;
 
   return logEntryElement;
-}
-
-// Save the log entry that is currently in the editor.
-void saveLog(Event _) {
-  DivElement logEntryElement = captureEditorView();
-  appendToLog(calcStardate(), logEntryElement);
-
-  // Clear the editor.
-  quillEditor.deleteText(0, quillEditor.getLength());
-}
-
-// Add an entry to the log.
-void appendToLog(double stardate, HtmlElement logEntryElement) {
-  logEntries[stardate] = logEntryElement;
-  window.localStorage[stardate.toString()] = logEntryElement.innerHtml;
-  displayLogEntry(stardate, logEntryElement);
 }
 
 // Show the log entry on the page.
@@ -88,14 +74,6 @@ void displayLogEntry(double stardate, HtmlElement logEntryElement) {
   logElement.insertAdjacentElement('afterBegin', stardateElement);
 }
 
-// Calculate the current stardate: <Year>.<Percentage of year completion>
-double calcStardate() {
-  DateTime now = new DateTime.now();
-  DateTime beginningOfYear = new DateTime(now.year);
-  int secondsThisYear = now.difference(beginningOfYear).inSeconds;
-  return now.year + secondsThisYear / secondsInAYear;
-}
-
 // Load all entries from local storage.
 void loadPreviousEntries() {
   Element logElement = document.getElementById('log');
@@ -109,4 +87,26 @@ void loadPreviousEntries() {
     logEntries[double.parse(key)] = entryElement;
     displayLogEntry(double.parse(key), entryElement);
   }
+}
+
+// Save the log entry that is currently in the editor.
+void saveLog(Event _) {
+  DivElement logEntryElement = captureEditorView();
+  appendToLog(calcStardate(), logEntryElement);
+
+  // Clear the editor.
+  quillEditor.deleteText(0, quillEditor.getLength());
+}
+
+// Updates the content of the editor using a template.
+void useTemplate(Event _) {
+  SelectElement templateSelectElement =
+      document.getElementById("templateSelect");
+  int selectedIndex = templateSelectElement.selectedIndex;
+
+  if (selectedIndex == 0) return;
+
+  quillEditor.deleteText(0, quillEditor.getLength());
+  String templateText = templates[templateSelectElement.selectedIndex - 1];
+  quillEditor.insertText(0, templateText, 'api');
 }
