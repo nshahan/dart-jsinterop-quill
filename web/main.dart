368528f4788fee0856ecfb7b27a4c47445a244cc
nshahan <nshahan@google.com>
2016-10-06 15:38:13 -0700

    Dart style cleanup.
    Fix style for dart code based on recomendations from NBosch and the Dart
    style guide.

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,11 @@
-import 'quill.dart' as quill;
-
 import 'dart:html';
 
+import 'quill.dart' as quill;
+
 // ignoring leap years
-final int secondsInAYear = 31536000;
-final String prompt = 'Something happened. Make it sound puzzling and heroic.';
-final List<String> templates = [
+const int _secondsInAYear = 31536000;
+const String _prompt = 'Something happened. Make it sound puzzling and heroic.';
+final List<String> _templates = [
   'We encountered what appeared to be a <insert space anomaly>. It has '
       'proven to be sentient and has taken control of our ship. Thus far, all '
       'efforts at communication have failed...',
@@ -23,44 +23,43 @@ Map<double, HtmlElement> logEntries;
 main() {
   // initialization
   quillEditor = new quill.QuillStatic('#editor',
-      new quill.QuillOptionsStatic(theme: 'snow', placeholder: prompt));
+      new quill.QuillOptionsStatic(theme: 'snow', placeholder: _prompt));
 
   logEntries = new Map<double, HtmlElement>();
   loadPreviousEntries();
 
   // listeners
   document.getElementById('save').onClick.listen(saveLog);
-  SelectElement foo = document.getElementById('templateSelect');
-  foo.onChange.listen(useTemplate);
+  document.getElementById('templateSelect') as SelectElement
+    ..onChange.listen(useTemplate);
 }
 
-// Add an entry to the log.
+/// Capture entry in editor, save to local storage and display in log.
 void appendToLog(double stardate, HtmlElement logEntryElement) {
   logEntries[stardate] = logEntryElement;
   window.localStorage[stardate.toString()] = logEntryElement.innerHtml;
   displayLogEntry(stardate, logEntryElement);
 }
 
-// Calculate the current stardate: <Year>.<Percentage of year completion>
-double calcStardate() {
-  DateTime now = new DateTime.now();
-  DateTime beginningOfYear = new DateTime(now.year);
+/// Calculate the current stardate: <Year>.<Percentage of year completion>
+double calculateStardate() {
+  var now = new DateTime.now();
+  var beginningOfYear = new DateTime(now.year);
   int secondsThisYear = now.difference(beginningOfYear).inSeconds;
-  return now.year + secondsThisYear / secondsInAYear;
+  return now.year + secondsThisYear / _secondsInAYear;
 }
 
-// copy html elements from the editor view and return them inside a new
-// DivElement.
+/// Copy html elements from the editor view and return them inside a new
+/// DivElement.
 HtmlElement captureEditorView() {
   Element contentElement = document.getElementById('editor').firstChild;
 
-  HtmlElement logEntryElement = new DivElement();
-  logEntryElement.innerHtml = contentElement.innerHtml;
+  var logEntryElement = new DivElement()
+    ..innerHtml = contentElement.innerHtml;
 
   return logEntryElement;
 }
 
-// Show the log entry on the page.
 void displayLogEntry(double stardate, HtmlElement logEntryElement) {
   Element logElement = document.getElementById('log');
 
@@ -75,7 +74,7 @@ void displayLogEntry(double stardate, HtmlElement logEntryElement) {
   logElement.insertAdjacentElement('afterBegin', stardateElement);
 }
 
-// Load all entries from local storage.
+/// Load all log entries from browser local storage.
 void loadPreviousEntries() {
   Element logElement = document.getElementById('log');
   logElement.innerHtml = window.localStorage['log'] ?? '';
@@ -83,31 +82,31 @@ void loadPreviousEntries() {
   List<String> keys = window.localStorage.keys.toList();
   keys.sort();
   for (String key in keys) {
-    DivElement entryElement = new DivElement();
-    entryElement.innerHtml = window.localStorage[key];
+    var entryElement = new DivElement()
+      ..innerHtml = window.localStorage[key];
     logEntries[double.parse(key)] = entryElement;
     displayLogEntry(double.parse(key), entryElement);
   }
 }
 
-// Save the log entry that is currently in the editor.
+/// Save the log entry that is currently in the editor.
 void saveLog(Event _) {
   DivElement logEntryElement = captureEditorView();
-  appendToLog(calcStardate(), logEntryElement);
+  appendToLog(calculateStardate(), logEntryElement);
 
   // Clear the editor.
   quillEditor.deleteText(0, quillEditor.getLength());
 }
 
-// Updates the content of the editor using a template.
+/// Updates the content of the editor using the selected template.
 void useTemplate(Event _) {
   SelectElement templateSelectElement =
-      document.getElementById("templateSelect");
+      document.getElementById('templateSelect');
   int selectedIndex = templateSelectElement.selectedIndex;
 
   if (selectedIndex == 0) return;
 
   quillEditor.deleteText(0, quillEditor.getLength());
-  String templateText = templates[templateSelectElement.selectedIndex - 1];
+  String templateText = _templates[templateSelectElement.selectedIndex - 1];
   quillEditor.insertText(0, templateText, 'api');
 }
